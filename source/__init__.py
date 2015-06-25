d0a97b5822f6fb64e76920598d02722925524b03
Matt <mattrock000@outlook.com>
2015-06-25 15:56:20 -0400

    Fade Security

---
+++ source/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-"
+"""Fade - is a tool that provides you and your network optimal security. 
+It monitors keyloggers, ddos attacks, and much more. 
+Fade also allows you to catch, trap, and back fire on those killing or interrupting your network through secure 
+and fast ICMP Flood Attacks, high powered DDOS Attacks, and much more!
+"""
